3e025627cef451693371945e8360dfb83ca46be9
Kalebe Chimanski de Almeida <87911732+Kalebe16@users.noreply.github.com>
2025-10-18 13:37:44 -0300

    refactor: Reorder methods and improve code structure clarity (#19)

--- restiny/core/request_area.py
+++ restiny/core/request_area.py
@@ -215,8 +215,16 @@ class RequestArea(Static):
             '#options-verify-ssl', Switch
         )
 
+    def get_data(self) -> RequestAreaData:
+        return RequestAreaData(
+            headers=self._get_headers(),
+            query_params=self._get_query_params(),
+            body=self._get_body(),
+            options=self._get_options(),
+        )
+
     @on(Select.Changed, '#body-mode')
-    def on_change_body_type(self, message: Select.Changed) -> None:
+    def _on_change_body_type(self, message: Select.Changed) -> None:
         if message.value == BodyMode.FILE:
             self.body_mode_switcher.current = 'body-mode-file'
         elif message.value == BodyMode.RAW:
@@ -227,27 +235,27 @@ class RequestArea(Static):
             self.body_mode_switcher.current = 'body-mode-form-multipart'
 
     @on(Select.Changed, '#body-raw-language')
-    def on_change_body_text_language(self, message: Select.Changed) -> None:
+    def _on_change_body_raw_language(self, message: Select.Changed) -> None:
         self.body_raw_editor.language = message.value
 
     @on(DynamicFields.FieldFilled, '#body-form-urlencoded')
-    def on_form_filled(self, message: DynamicFields.FieldFilled) -> None:
+    def _on_form_filled(self, message: DynamicFields.FieldFilled) -> None:
         self.body_enabled_switch.value = True
 
     @on(DynamicFields.FieldEmpty, '#body-form-urlencoded')
-    def on_form_empty(self, message: DynamicFields.FieldEmpty) -> None:
+    def _on_form_empty(self, message: DynamicFields.FieldEmpty) -> None:
         if not message.control.filled_fields:
             self.body_enabled_switch.value = False
 
     @on(CustomTextArea.Changed, '#body-raw')
-    def on_change_body_text(self, message: CustomTextArea.Changed) -> None:
+    def _on_change_body_raw(self, message: CustomTextArea.Changed) -> None:
         if self.body_raw_editor.text == '':
             self.body_enabled_switch.value = False
         else:
             self.body_enabled_switch.value = True
 
     @on(Input.Changed, '#options-timeout')
-    def on_change_timeout(self, message: Input.Changed) -> None:
+    def _on_change_timeout(self, message: Input.Changed) -> None:
         new_value = message.value
 
         if new_value == '':
@@ -260,80 +268,70 @@ class RequestArea(Static):
                 self.options_timeout_input.value[:-1]
             )
 
-    def get_data(self) -> RequestAreaData:
-        def get_headers() -> list[HeaderField]:
-            return [
-                HeaderField(
-                    enabled=header_field['enabled'],
-                    key=header_field['key'],
-                    value=header_field['value'],
-                )
-                for header_field in self.header_fields.values
-            ]
-
-        def get_query_params() -> list[QueryParamField]:
-            return [
-                QueryParamField(
-                    enabled=query_param_field['enabled'],
-                    key=query_param_field['key'],
-                    value=query_param_field['value'],
-                )
-                for query_param_field in self.param_fields.values
-            ]
-
-        def get_body() -> RequestAreaData.Body:
-            body_send: bool = self.body_enabled_switch.value
-            body_type: str = BodyMode(self.body_mode_select.value)
-
-            payload = None
-            if body_type == BodyMode.RAW:
-                payload = self.body_raw_editor.text
-            elif body_type == BodyMode.FILE:
-                payload = self.body_file_path_chooser.path
-            elif body_type == BodyMode.FORM_URLENCODED:
-                payload = []
-                for form_item in self.body_form_urlencoded_fields.values:
-                    payload.append(
-                        FormUrlEncodedField(
-                            enabled=form_item['enabled'],
-                            key=form_item['key'],
-                            value=form_item['value'],
-                        )
-                    )
-            elif body_type == BodyMode.FORM_MULTIPART:
-                payload = []
-                for form_item in self.body_form_multipart_fields.values:
-                    payload.append(
-                        FormMultipartField(
-                            enabled=form_item['enabled'],
-                            key=form_item['key'],
-                            value=form_item['value'],
-                        )
-                    )
-
-            return RequestAreaData.Body(
-                enabled=body_send,
-                raw_language=BodyRawLanguage(
-                    self.body_raw_language_select.value
-                ),
-                type=body_type,
-                payload=payload,
+    def _get_headers(self) -> list[HeaderField]:
+        return [
+            HeaderField(
+                enabled=header_field['enabled'],
+                key=header_field['key'],
+                value=header_field['value'],
             )
+            for header_field in self.header_fields.values
+        ]
 
-        def get_options() -> RequestAreaData.Options:
-            timeout = None
-            if self.options_timeout_input.value:
-                timeout = float(self.options_timeout_input.value)
-
-            return RequestAreaData.Options(
-                timeout=timeout,
-                follow_redirects=self.options_follow_redirects_switch.value,
-                verify_ssl=self.options_verify_ssl_switch.value,
+    def _get_query_params(self) -> list[QueryParamField]:
+        return [
+            QueryParamField(
+                enabled=query_param_field['enabled'],
+                key=query_param_field['key'],
+                value=query_param_field['value'],
             )
+            for query_param_field in self.param_fields.values
+        ]
 
-        return RequestAreaData(
-            headers=get_headers(),
-            query_params=get_query_params(),
-            body=get_body(),
-            options=get_options(),
+    def _get_body(self) -> RequestAreaData.Body:
+        body_send: bool = self.body_enabled_switch.value
+        body_type: str = BodyMode(self.body_mode_select.value)
+
+        payload = None
+        if body_type == BodyMode.RAW:
+            payload = self.body_raw_editor.text
+        elif body_type == BodyMode.FILE:
+            payload = self.body_file_path_chooser.path
+        elif body_type == BodyMode.FORM_URLENCODED:
+            payload = []
+            for form_item in self.body_form_urlencoded_fields.values:
+                payload.append(
+                    FormUrlEncodedField(
+                        enabled=form_item['enabled'],
+                        key=form_item['key'],
+                        value=form_item['value'],
+                    )
+                )
+        elif body_type == BodyMode.FORM_MULTIPART:
+            payload = []
+            for form_item in self.body_form_multipart_fields.values:
+                payload.append(
+                    FormMultipartField(
+                        enabled=form_item['enabled'],
+                        key=form_item['key'],
+                        value=form_item['value'],
+                    )
+                )
+
+        return RequestAreaData.Body(
+            enabled=body_send,
+            raw_language=BodyRawLanguage(self.body_raw_language_select.value),
+            type=body_type,
+            payload=payload,
+        )
+
+    def _get_options(self) -> RequestAreaData.Options:
+        timeout = None
+        if self.options_timeout_input.value:
+            timeout = float(self.options_timeout_input.value)
+
+        return RequestAreaData.Options(
+            timeout=timeout,
+            follow_redirects=self.options_follow_redirects_switch.value,
+            verify_ssl=self.options_verify_ssl_switch.value,
         )
