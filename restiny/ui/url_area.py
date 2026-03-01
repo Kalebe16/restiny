4b1a6716531ed3b414f726a41b409e608d770c0d
Kalebe Chimanski de Almeida <87911732+Kalebe16@users.noreply.github.com>
2026-03-01 08:29:56 -0300

    feat: Response download (#63)

--- restiny/ui/url_area.py
+++ restiny/ui/url_area.py
@@ -2,7 +2,7 @@ from textual import on
 from textual.app import ComposeResult
 from textual.containers import Horizontal
 from textual.message import Message
-from textual.widgets import Button, ContentSwitcher, Select, Static
+from textual.widgets import Button, ContentSwitcher, Rule, Select, Static
 
 from restiny.enums import HTTPMethod
 from restiny.widgets import CustomInput
@@ -24,6 +24,14 @@ class URLArea(Static):
     class SendRequest(Message):
         """
         Sent when the user send a request.
+        """
+
+        def __init__(self) -> None:
+            super().__init__()
+
+    class DownloadResponse(Message):
+        """
+        Sent when the user download a response.
         """
 
         def __init__(self) -> None:
@@ -56,30 +64,42 @@ class URLArea(Static):
                 id='url',
             )
             with ContentSwitcher(
-                id='request-button-switcher', initial='send-request'
+                id='request-controls-switcher', initial='request-actions'
             ):
+                with Horizontal(classes='h-auto w-auto', id='request-actions'):
+                    yield Button(
+                        label='Send',
+                        id='send',
+                        classes='w-1fr',
+                        variant='default',
+                    )
+                    yield Rule(orientation='vertical', classes='h-auto m-0')
+                    yield Button(
+                        label='Download',
+                        id='download',
+                        classes='w-1fr',
+                        variant='default',
+                    )
                 yield Button(
-                    label='Send Request',
-                    id='send-request',
-                    classes='w-1fr',
-                    variant='default',
-                )
-                yield Button(
-                    label='Cancel Request',
-                    id='cancel-request',
+                    label='Cancel',
+                    id='cancel',
                     classes='w-1fr',
                     variant='error',
                 )
 
     def on_mount(self) -> None:
-        self._request_button_switcher = self.query_one(
-            '#request-button-switcher', ContentSwitcher
+        self._request_controls_switcher = self.query_one(
+            '#request-controls-switcher', ContentSwitcher
         )
 
         self.method_select = self.query_one('#method', Select)
         self.url_input = self.query_one('#url', CustomInput)
-        self.send_request_button = self.query_one('#send-request', Button)
-        self.cancel_request_button = self.query_one('#cancel-request', Button)
+        self.request_actions_container = self.query_one(
+            '#request-actions', Horizontal
+        )
+        self.send_button = self.query_one('#send', Button)
+        self.download_button = self.query_one('#download', Button)
+        self.cancel_button = self.query_one('#cancel', Button)
 
     @property
     def request_pending(self) -> bool:
@@ -88,9 +108,9 @@ class URLArea(Static):
     @request_pending.setter
     def request_pending(self, value: bool) -> None:
         if value is True:
-            self._request_button_switcher.current = 'cancel-request'
+            self._request_controls_switcher.current = 'cancel'
         elif value is False:
-            self._request_button_switcher.current = 'send-request'
+            self._request_controls_switcher.current = 'request-actions'
 
         self._request_pending = value
 
@@ -114,9 +134,9 @@ class URLArea(Static):
         self.method = HTTPMethod.GET
         self.url = ''
 
-    @on(Button.Pressed, '#send-request')
+    @on(Button.Pressed, '#send')
     @on(CustomInput.Submitted, '#url')
-    def _on_send_request(
+    def _on_send(
         self, message: Button.Pressed | CustomInput.Submitted
     ) -> None:
         if self.request_pending:
@@ -124,9 +144,16 @@ class URLArea(Static):
 
         self.post_message(message=self.SendRequest())
 
-    @on(Button.Pressed, '#cancel-request')
+    @on(Button.Pressed, '#download')
+    def _on_download(self, message: Button.Pressed) -> None:
+        if self.request_pending:
+            return
+
+        self.post_message(message=self.DownloadResponse())
+
+    @on(Button.Pressed, '#cancel')
     @on(CustomInput.Submitted, '#url')
-    def _on_cancel_request(self, message: Button.Pressed) -> None:
+    def _on_cancel(self, message: Button.Pressed) -> None:
         if not self.request_pending:
             return
 
